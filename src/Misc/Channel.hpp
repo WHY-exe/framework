a11c1a49d00420f5409afbbab6103615622cf25c
That_Individual Why <2947875090@qq.com>
2026-09-06 22:22:43 +0800

    adding mpcs channel

--- src/Misc/Channel.hpp
+++ src/Misc/Channel.hpp
@@ -2,7 +2,10 @@
 #include "NameHelper.h"
 #include <boost/asio/dispatch.hpp>
 #include <boost/asio/steady_timer.hpp>
+#include <condition_variable>
+#include <mutex>
 #include <queue>
+#include "Error.h"
 
 namespace misc {
 template <typename T>
@@ -13,13 +16,15 @@ T PopValue(std::queue<T>& queue) {
 }
 
 template <typename T, typename Executor>
-class Channel {
+class AsyncChannel {
 	using executor_type = Executor;
 
 public:
-	explicit Channel(const executor_type& ex, size_t maxSize)
+	explicit AsyncChannel(const executor_type& ex, size_t maxSize)
 		: m_tmChanFull(ex)
-		, m_tmChanRecv(ex) {
+		, m_tmChanRecv(ex)
+		, m_maxSize(maxSize)
+		, m_close(false) {
 	}
 
 	template <class CompletionToken>
@@ -39,19 +44,19 @@ public:
 							std::move(handler)(ec);
 							return;
 						}
-                        bool signalRecv = m_queue.empty();
+						bool signalRecv = m_queue.empty();
 						m_queue.push(std::forward<T>(value));
 						std::move(handler)({});
-                        if (signalRecv) {
-                            m_tmChanRecv.cancel();
-                        }
+						if (signalRecv) {
+							m_tmChanRecv.cancel();
+						}
 					});
 			},
 			token);
 	}
 
 	template <class CompletionToken>
-	auto Send(CompletionToken&& token) {
+	auto Recv(CompletionToken&& token) {
 		using Signature = void(ErrorCode, T);
 		return asio::async_initiate<CompletionToken, Signature>(
 			[this](auto&& handler) mutable {
@@ -67,12 +72,12 @@ public:
 							std::move(handler)(ec, {});
 							return;
 						}
-                        bool signalSend = m_queue.size() == m_maxSize;
-						auto value = PopValue(m_queue);
+						bool signalSend = m_queue.size() == m_maxSize;
+						auto value		= PopValue(m_queue);
 						std::move(handler)({}, std::move(value));
 						if (signalSend) {
-                            m_tmChanFull.cancel();
-                        }
+							m_tmChanFull.cancel();
+						}
 					});
 			},
 			token);
@@ -94,5 +99,58 @@ private:
 	std::queue<T>	   m_queue;
 	size_t			   m_maxSize;
 	std::atomic_bool   m_close;
+};
+
+template <typename T, typename Executor>
+class MPSCChannel {
+public:
+	explicit MPSCChannel(size_t maxSize)
+		: m_maxSize(maxSize)
+		, m_close(false) {
+	}
+
+	Result<void> Send(T&& value) {
+		if (m_close) {
+			return MAKE_EC(boost::system::errc::make_error_code(boost::system::errc::operation_canceled));
+		}
+		std::unique_lock lk(m_mutex);
+		if (m_queue.size() >= m_maxSize) {
+			m_cvFul.wait(lk, [this] { return m_queue < m_maxSize; });
+		}
+		bool bEmpty = m_queue.empty();
+		m_queue.push(std::forward<T>(value));
+		if (bEmpty) {
+			m_cvEmpty.notify_one();
+		}
+		return {};
+	}
+
+	Result<T> Recv() {
+		if (m_close) {
+			return MAKE_EC(boost::system::errc::make_error_code(boost::system::errc::operation_canceled));
+		}
+		std::unique_lock lk(m_mutex);
+		if (m_queue.empty()) {
+			m_cvEmpty.wait(lk, [this] { return !m_queue.empty(); });
+		}
+		bool bFull = m_queue.size() >= m_maxSize;
+		auto ret = PopValue(m_queue);
+		if (bFull) {
+			m_cvFul.notify_all();
+		}
+		return ret;
+	}
+
+	void Close() {
+		m_close.exchange(true);
+	}
+
+private:
+	std::mutex				m_mutex;
+	std::condition_variable m_cvFul;
+	std::condition_variable m_cvEmpty;
+	std::queue<T>			m_queue;
+	size_t					m_maxSize;
+	std::atomic_bool		m_close;
 };
 } //namespace misc
